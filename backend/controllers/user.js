17b2ebf101c352549dc128fa504ee8415177b3b0
Jane49-cloud <janendirangu49@gmail.com>
2022-11-13 00:16:20 +0300

    User registration validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,19 +1,37 @@
-const { response } = require('express');
-const User = require('../models/user')
+const { response } = require("express");
+const User = require("../models/user");
 
-const registerUser = async(req, res)=>{
-    try {
-        const user = await User.create(req.body)
-        res.json(user)
-       console.log(user, "User created successfully")
-    } catch (error) {
-    console.log(error , `error registering`);
-   
+const registerUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+
+    //validation
+    if (!req.body.name || !req.body.email || !req.body.password) {
+      res.status(400);
+      throw new Error("Please fill in the required fields");
     }
-}
 
+    if (req.body.password.length < 6) {
+      throw new Error("The password must be at least 6 characters");
+    }
 
+    // Check if user exists
+    const {email } = req.body.email;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      response.status(400);
+      throw new Error("Email address already exists");
+    }
+
+    //Send back data
+    res.json(user);
+    console.log(user, "User created successfully");
+  } catch (error) {
+    console.log(error, `error registering`);
+  }
+};
 
 module.exports = {
-    registerUser,
-}
+  registerUser,
+};
