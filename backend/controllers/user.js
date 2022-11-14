57c5d70447536efad2988ed0b52f59ba0de0bca4
Jane49-cloud <janendirangu49@gmail.com>
2022-11-14 15:42:58 +0300

    validate and login user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const asyncHandler =require('express-async-handler')
+const bcrypt = require('bcryptjs')
 
 //Generate a token
 const generateToken = (id) => {
@@ -62,11 +63,38 @@ const registerUser = async (req, res) => {
 
 // login user also start using async-handler
 
-const loginUser = asyncHandler( async (req, res) =>{
-    res.send("login user");
+const loginUser= async (req, res) =>{
+  const {email, password} = req.body;
+
+  //validate request
+  if(!email || !password){
+    res.status(400)
+    throw new Error('please enter add email and password')
+  }
+  // Check if user exists
+   const user = await User.findOne({email})
+   
+   if(!user){
+    res.status(400)
+    throw new Error("User with that email not found , please register")
+   }
+   // user exists, Check if the password is correct
+   const passwordCorrect = await bcrypt.compare(password, user.password)
+
+   if(user && passwordCorrect){
+    const user  = req.body
+    res.status(200).json(
+        {
+            user
+         }
+    )
+   }
+   else{
+     res.status(400)
+    throw new Error("password or email incorrect")
+   }
 
 }
-)
 
 module.exports = {
   registerUser,
