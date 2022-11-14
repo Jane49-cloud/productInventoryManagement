49504ef8002fa9d89d415747aaf68c2558e0e3ad
Jane49-cloud <janendirangu49@gmail.com>
2022-11-14 14:09:27 +0300

    Generating jwtwebtoken

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,46 +1,59 @@
 const { response } = require("express");
 const User = require("../models/user");
-const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+
+//Generate a token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+}
 
 const registerUser = async (req, res) => {
-  try {
+    try {
 
-    const { email, password , name } = req.body
+        const { email, password, name } = req.body
 
 
-    // Check if user exists
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      response.status(400);
-      throw new Error("Email address already exists");
+        // Check if user exists
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            response.status(400);
+            throw new Error("Email address already exists");
+        }
+
+
+        const user = await User.create({
+            name,
+            password,
+            email
+        });
+
+         //generate a 
+             const token = generateToken(user._id)
+
+
+        //validation
+        if (!req.body.name || !req.body.email || !req.body.password) {
+            res.status(400);
+            throw new Error("Please fill in the required fields");
+        }
+
+        if (req.body.password.length < 6) {
+            throw new Error("The password must be at least 6 characters");
+        }
+
+
+        //Send back data
+        res.status(201).json({
+            user,
+            token
+        })
+        console.log(user, "User created successfully");
+    } catch (error) {
+        console.log(error, `error registering`);
     }
-
-
-    const user = await User.create({
-        name,
-        password,
-        email
-    });
-
-    //validation
-    if (!req.body.name || !req.body.email || !req.body.password) {
-      res.status(400);
-      throw new Error("Please fill in the required fields");
-    }
-
-    if (req.body.password.length < 6) {
-      throw new Error("The password must be at least 6 characters");
-    }
-
-    
-    //Send back data
-    res.json(user);
-    console.log(user, "User created successfully");
-  } catch (error) {
-    console.log(error, `error registering`);
-  }
 };
 
 module.exports = {
-  registerUser,
+    registerUser,
 };
